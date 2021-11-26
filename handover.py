6a5844f3e1cc32d7ad207751f4cacd9317e8eb92
Ama-n <75764672+Ama-n@users.noreply.github.com>
2021-11-26 12:49:31 +0530

    Update handover.py

--- handover.py
+++ handover.py
@@ -18,7 +18,7 @@ st.set_page_config(page_title='SWAST - Handover Delays',  layout='wide', page_ic
 
 t1, t2 = st.columns((0.07,1)) 
 
-t1.image('images/index.png', width = 120)
+t1.image('index.png', width = 120)
 t2.title("South Western Ambulance Service - Hospital Handover Report")
 t2.markdown(" **tel:**9087654321  **| email:** mailto:priya@gmail.com")
 
